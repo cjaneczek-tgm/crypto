7ba5351a93c1bd6757b959081ee7d81161cc3630
Chris <cjaneczek@student.tgm.ac.at>
2015-01-29 15:21:57 +0100

    Debugging AESBehavior's InvalidKeyException

--- src/main/java/crypto/env/start/StartEncryption.java
+++ src/main/java/crypto/env/start/StartEncryption.java
@@ -1,26 +1,30 @@
 package crypto.env.start;
 
 import crypto.cipher.cbehavior.AESBehavior;
-import crypto.cipher.cbehavior.PTBehavior;
-import crypto.cipher.cbehavior.RSABehavior;
-import crypto.env.Client;
-import crypto.env.Server;
-
-import java.security.KeyPair;
+import org.apache.log4j.Logger;
 
 public class StartEncryption {
 
-	public void main(String[] args) {
+	public static void main(String[] args) {
 
-		String pseudoServerName = "localhost";
-		String pseudoPublicKey = "01001010101";
-		String pseudoSharedKey = "10110101001";
-		KeyPair keyPair;
+        Logger logger = org.apache.log4j.Logger.getLogger(StartEncryption.class);
+//		String pseudoServerName = "localhost";
+//		String pseudoPublicKey = "01001010101";
+//		String pseudoSharedKey = "10110101001";
+//		KeyPair keyPair;
+//
+//		int pseudoServerPort = 8080;
+//		Server server = new Server(8080, new AESBehavior());
+//		Client client = new Client(pseudoServerName, pseudoServerPort, new AESBehavior());
+//		keyPair = server.generatePairedKey();
+//		server.encryptString(server.getPublicKey(),keyPair.getPublic().toString());
+        AESBehavior aesBehavior = new AESBehavior();
 
-		int pseudoServerPort = 8080;
-		Server server = new Server(8080, new AESBehavior());
-		Client client = new Client(pseudoServerName, pseudoServerPort, new AESBehavior());
-		keyPair = server.generatePairedKey();
-		server.encryptString(server.getPublicKey(),keyPair.getPublic().toString());
+        String message = "Hello, I am a message!";
+        String key = aesBehavior.generateKey().toString();
+        logger.info("Message: '"+message+"'");
+        logger.info("Key: "+key);
+        logger.info(aesBehavior.encryptString(message, key));
+
 	}
 }
